4ca70f648750f521ecd90e3ab000649292869511
HXABOOD9 <hxabood9@gmail.com>
2025-04-09 15:19:34 +0400

    Update rivals_script.lua

--- rivals_script.lua
+++ rivals_script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yourusername/yourrepo/main/rivals_script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HXABOOD9/RIVALS/main/rivals_script.lua"))()
